69d3b0bf4b1e5e9eaf45fa5b5ddab9e7090a180c
Ivan.Calzadas <Ivan.Calzadas@mecalux.com>
2025-07-22 11:57:13 -0600

    agrego left sidebar (omo el de devops)

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from '../../shared/components/top-bar/top-bar.component';
+import { LeftSidebarComponent } from '../../shared/components/left-sidebar/left-sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [TopBarComponent, RouterOutlet],
+  imports: [TopBarComponent, LeftSidebarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
