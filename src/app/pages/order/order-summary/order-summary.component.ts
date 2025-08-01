69ceebbaf167b14c4c351a66ed7615f5b01a65af
Ivan.Calzadas <Ivan.Calzadas@mecalux.com>
2025-08-01 16:55:52 -0600

    product left side

--- src/app/pages/order/order-summary/order-summary.component.ts
+++ src/app/pages/order/order-summary/order-summary.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css'
 })
 export class OrderSummaryComponent {
+
+  items = [
+    { name: 'Item 1', quantity: 1, price: 50 },
+    { name: 'Item 2', quantity: 2, price: 30 }
+  ];
+
+  increaseQuantity(item: any) {
+    item.quantity++;
+  }
+
+  decreaseQuantity(item: any) {
+    if (item.quantity > 1) item.quantity--;
+  }
+
+  getTotal(): number {
+    return this.items.reduce((sum, item) => sum + (item.quantity * item.price), 0);
+  }
 
 }
